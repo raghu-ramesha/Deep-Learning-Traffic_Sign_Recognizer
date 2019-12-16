f8bd011618f0219be35aa365187ba09f9ce5d1d1
Raghunandan Ramesha <rags1357@gmail.com>
2019-12-16 09:50:22 -0800

    Update benchmark.py

--- predict/benchmark.py
+++ predict/benchmark.py
@@ -1,15 +1,11 @@
 from predict.featureextractor import FeatureExtractor
 
-__author__ = 'Group16'
-
 """
 
     This class contains the benchmark predictor. For the training data, calculate the occurrence of each traffic
     sign and assign these occurrence probabilities to the new test data. Conclusion: it's really really bad,
     don't do worse!
 
-    Written by Group 16: Tim Deweert, Karsten Goossens & Gilles Vandewiele
-    Commissioned by UGent, course Machine Learning
 
 """
 
